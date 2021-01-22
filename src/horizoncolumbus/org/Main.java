465e15f2d8190ef5765e72ec6e82eccec75fb49f
Sabriyes Project <student.sbr.mohamed7@horizoncolumbus.org>
2021-01-21 21:00:34 -0500

    Initial commit

---
+++ src/horizoncolumbus/org/Main.java
@@ -0,0 +1,24 @@
+package horizoncolumbus.org;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] numbers = new int[5];
+        for (int i = 0; i < 5; i++){
+            numbers[i] = i + 1;
+
+        int sum = 0;
+
+            for (int i = 0; i < 5; i++){
+                sum = sum+numbers[i];
+
+
+            // fpr (int i; i < 5; i++)
+                // above shows that if I do not initialize i, it will still show an error saying it is not initialized,
+                //but when I do initialize it, it shows that it was already done before
+            }
+            System.out.println(sum);
+        }
+    }
+}
